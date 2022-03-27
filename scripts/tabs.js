90c1b7449b3d20f73412a398288957a85b291d5c
Marina Belova <83077065+Salirsky@users.noreply.github.com>
2022-03-27 20:53:05 +0300

    Add script tabs

--- scripts/tabs.js
+++ scripts/tabs.js
@@ -1,1 +1,30 @@
+const tabButtons = document.querySelectorAll(".menu-item_btn");
+const tabDescriptions = document.querySelectorAll(".menu-content");
+// const tabImages = document.querySelectorAll(".design-images");
+const changeContent = (array, value) => {
+  array.forEach((elem) => {
+    if (elem.dataset.tabsField === value) {
+      elem.classList.remove("hidden");
+    } else {
+      elem.classList.add("hidden");
+    }
+  });
+};
+
+tabButtons.forEach((tabButton) => {
+  tabButton.addEventListener("click", (event) => {
+    const dataValue = tabButton.dataset.tabsHandler;
+
+    changeContent(tabDescriptions, dataValue);
+    // changeContent(tabImages, dataValue);
+
+    tabButtons.forEach((btn) => {
+      if (btn === event.target) {
+        btn.classList.add("menu-item_btn_active");
+      } else {
+        btn.classList.remove("menu-item_btn_active");
+      }
+    });
+  });
+});
 
